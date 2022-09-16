099119072713febd84e5ccce500f5c3882e22b80
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-16 12:40:52 -0300

    feat: criando testes na camada controllers para busca pelo id

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -7,13 +7,11 @@ const { allProducts, productById } = require('../../mocks/mockProducts');
 
 describe('testes na camada Controllers', () => {
   describe('testa a resposta da chamada da lista de produtos', () => {
-    
     const req = {};
     const res = {};
-
+  
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns();
-
     before(async () => {
       sinon.stub(productsServices, 'getProducts').resolves(allProducts);
     })
@@ -22,9 +20,29 @@ describe('testes na camada Controllers', () => {
       productsServices.getProducts.restore();
     })
 
-    it('Testa se o retorno é um array ', async () => {
+    it('Testa se a chamada da lista de produtos é feita corretamente  ', async () => {
       await productsControllers.getAllProducts(req, res);
       expect(res.status.calledWith(200)).to.be.equal(true);
     })
   })
+
+  describe('testa a resposta da chamada do produto por ID', () => {
+    const req = {params: {id:1}};
+    const res = {};
+  
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    before(async () => {
+      sinon.stub(productsServices, 'getProductsId').resolves(productById);
+    })
+
+    after(async () => {
+      productsServices.getProductsId.restore();
+    })
+
+    it('Testa se a chamada de um produto pelo Id é feita corretamente', async () => {
+      await productsControllers.getProductsById(req, res);
+      expect(res.status.calledWith(200)).to.be.equal(true);
+    })
+  });
 });
