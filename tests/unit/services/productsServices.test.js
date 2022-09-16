cdd5b69c228287e6ef9f3757036b7727f2404b5c
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-16 12:03:30 -0300

    feat: criando teste na cama services

---
+++ tests/unit/services/productsServices.test.js
@@ -0,0 +1,43 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const productsServices = require('../../../src/services/productsServices');
+const productsModel = require('../../../src/models/productsModel');
+const { allProducts, productById } = require('../../mocks/mockProducts');
+
+describe('testes na camada Services',async  () => {
+  describe('testa A listagem de Produtos',async () => { 
+    before(async () => {
+        sinon.stub(productsModel, 'findProducts').resolves(allProducts);
+    })
+
+    after(async () => {
+      productsModel.findProducts.restore();
+    })
+
+    it('Testa se o retorno é um array ', async () => {
+    const result = await productsServices.getProducts();
+    expect(result).to.be.a('array');
+    })
+    
+    it('Testa se a busca retorna o objeto de produtos completo', async () => {
+    const result = await productsServices.getProducts();
+    expect(result).to.be.deep.equal(allProducts);
+    })
+  }) 
+
+  describe('testa A listagem de um Produto buscado pelo Id', () => { 
+    before(async () => {
+        sinon.stub(productsModel, 'findProductsById').resolves(productById);
+    })
+
+    after(async () => {
+      productsModel.findProductsById.restore();
+    })
+
+    it('Testa se o retorno da busca de um produto por id na camada Service ', async () => {
+    const result = await productsServices.getProductsId(1);
+    expect(result).to.be.deep.equal(productById);
+    })
+  }) 
+});
