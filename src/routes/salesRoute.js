47d4c94d247695c59f4076b6dd6733962aa54657
jefersongjr <jeferson_gjr@hotmail.com>
2022-09-17 15:28:53 -0300

    fix: corrigindo lint

--- src/routes/salesRoute.js
+++
@@ -1,6 +0,0 @@
-const salesRoute = require('express').Router();
-const { getAllSales } = require('../controllers/salesControllers');
-
-salesRoute.post('/sales', getAllSales);
-
-module.exports = salesRoute;  
